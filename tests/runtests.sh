347151d98b98036dc17a83f3da616dba8e10de09
Vojtech Eichler <vojta.eich@gmail.com>
2021-10-24 13:34:37 +0200

    tests: add parsing of multiple input files for scanner testing

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-SCANNER_INPUT="scanner-input"
-SCANNER_OUTPUT="scanner-helper.out"
-SCANNER_EXPECTED="scanner-helper.expected"
+INPUT_FILES=$(ls scanner-tests | grep .input)
+TEST_DIR=scanner-tests/
 
-./scanner-helper < $SCANNER_INPUT > $SCANNER_OUTPUT
-diff $SCANNER_OUTPUT $SCANNER_EXPECTED
-if [ $? -eq 0 ]; then
-    echo "PASS"
-else
-    echo "FAIL"
-fi
+for f in $INPUT_FILES; do
+    TEST_NAME=$(echo $f | cut -d'.' -f1)
+    INPUT=$TEST_DIR$f
+    OUTPUT=$TEST_DIR$(echo $f | cut -d'.' -f1).output
+    EXPECTED=$TEST_DIR$(echo $f | cut -d'.' -f1).expected
+    DIFF=$TEST_DIR$(echo $f | cut -d'.' -f1).diff
+
+    # Run tests
+    ./scanner-helper < $TEST_DIR$f > $OUTPUT
+
+    # Check output
+    diff $OUTPUT $EXPECTED > $DIFF
+    echo "Testing: $TEST_NAME"
+    if [ $? -eq 0 ]; then
+        echo "PASS"
+    else
+        echo "FAIL - CHECK .diff FILE"
+    fi
+done
