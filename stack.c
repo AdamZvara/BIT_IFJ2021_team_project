a09e33d302356550d8d50c949cd37f7b5877782b
Booooozer <tomatus777@gmail.com>
2021-11-08 19:57:51 +0100

    Implemented stack functions in stack.c, added entry to .gitignore
    Signed-off-by: Booooozer <tomatus777@gmail.com>

---
+++ stack.c
@@ -0,0 +1,64 @@
+/**
+ * VUT IFJ Project 2021.
+ *
+ * @file stack.c
+ *
+ * @brief Implementation of stack for bottom up parsing
+ *
+ * @author Vojtěch Eichler
+ * @author Václav Korvas
+ * @author Tomáš Matuš
+ * @author Adam Zvara
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+void stack_init(stack_t *stack)
+{
+    stack->top = NULL;
+}
+
+int stack_is_empty(stack_t *stack)
+{
+    return stack->top == NULL;
+}
+
+int stack_push(stack_t *stack, int value)
+{
+    stack_item_t *new = malloc(sizeof(stack_item_t));
+    if (new == NULL) {
+        return 1;
+    }
+
+    new->data = value;
+    new->next = stack->top;
+    stack->top = new;
+
+    return 0;
+}
+
+int stack_pop(stack_t *stack)
+{
+    if (stack->top == NULL) {
+        // popping an empty stack
+        return 1;
+    }
+
+    stack_item_t *pop = stack->top;
+    stack->top = pop->next;
+    free(pop);
+
+    return 0;
+}
+
+stack_item_t *stack_top(stack_t *stack)
+{
+    return stack->top;
+}
+
+void stack_dispose(stack_t *stack)
+{
+    while (!stack_pop(stack)); // empty body
+}
