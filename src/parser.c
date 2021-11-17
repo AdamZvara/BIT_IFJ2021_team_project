b94608e85bc445ce43a990a85df513090af79f68
Vojtech Eichler <vojta.eich@gmail.com>
2021-11-17 13:26:47 +0100

    parser: implement rules for parameters and return parameters

--- src/parser.c
+++ src/parser.c
@@ -20,6 +20,7 @@
 #include "parser.h"
 
 token_t *curr_token = NULL;
+token_t *backup_token = NULL;
 int ret = SUCCESS;
 
 //int token_init(token_t *token) {
@@ -70,10 +71,14 @@ int require()
 
 int prog()
 {
-    FREE_TOK_STRING();
-    ret = get_token(curr_token);
-    if (ret)
-        return ret;
+    //TODO: check if we have backup token, if there is one, dont read new token and
+    // just copy backup_token to curr_token and make backup_token NULL
+    if (!backup_token) {
+        NEXT_TOKEN();
+    } else {
+        curr_token = backup_token;
+        backup_token = NULL;
+    }
     if (curr_token->type == TOK_KEYWORD) { // new token is keyword
         if (curr_token->attribute.keyword == KW_GLOBAL) { // check if keyword is _global_
             // get new token that should be ID
@@ -97,14 +102,16 @@ int prog()
             if (curr_token->type != TOK_LBRACKET)
                 return ERROR_SYNTAX;
 
-            //TODO: call params()
+            // call params rule, check exit code and return if params were not successful,
+            // also skip reading next token
+            ret = params();
+            if (ret)
+                return ret;
 
-            // get new token that should be closing bracket
-            NEXT_TOKEN();
-            if (curr_token->type != TOK_RBRACKET)
-                return ERROR_SYNTAX;
-
-            //TODO: call ret_params()
+            // step into <ret_params> rule
+            ret = ret_params();
+            if (ret)
+                return ret;
 
             return prog();
 
@@ -120,31 +127,171 @@ int prog()
                 return ERROR_SYNTAX;
 
             //TODO: call params_2()
+            ret = params_2();
+            if (ret)
+                return ret;
 
-            // get new token that should be closing bracket
-            NEXT_TOKEN();
-            if (curr_token->type != TOK_RBRACKET)
-                return ERROR_SYNTAX;
-
-            //TODO: call ret_params()
-
-            //TODO: call body()
-
-            // get new token that should be keyword end
-            NEXT_TOKEN();
-            if (curr_token->type != TOK_KEYWORD || curr_token->attribute.keyword != KW_END)
-                return ERROR_SYNTAX;
+            // step into <ret_params> rule
+            ret = ret_params();
+            if (ret)
+                return ret;
             
+            //TODO: call body()
+            // body has to check for end
+            // ret = body()
+            // if (ret)
+            //     return ret;
+
             return prog();
 
         } else { // unexpected keyword, return error
             return ERROR_SYNTAX;
         }
     } else if (curr_token->type == TOK_ID) { // new token is ID
+        NEXT_TOKEN();
+        if (curr_token->type != TOK_LBRACKET)
+            return ERROR_SYNTAX;
 
+        //TODO: call args()
+        
+        NEXT_TOKEN();
+        if (curr_token->type != TOK_RBRACKET)
+            return ERROR_SYNTAX;
+
+        return prog();
+
+    }  else if (curr_token->type == TOK_EOF) {
+        // end of input, start returning all the way up the recursion
+        return ret;
     } else { // unexpected token, return error
         return ERROR_SYNTAX;
     }
+}
+
+int params() {
+    NEXT_TOKEN();
+    if (curr_token->type == TOK_RBRACKET) {
+        return ret;
+    } else if ((curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_STRING) ||
+               (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_NUMBER) ||
+               (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_INTEGER)) {
+
+        return params_n();
+    } else {
+        return ERROR_SYNTAX;
+    }
+}
+
+int params_n() {
+    NEXT_TOKEN();
+    if (curr_token->type == TOK_RBRACKET) {
+        return ret;
+    } else if (curr_token->type == TOK_COMMA) {
+        NEXT_TOKEN();
+        if ((curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_STRING) ||
+               (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_NUMBER) ||
+               (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_INTEGER)) {
+
+            return params_n();
+        } else {
+            return ERROR_SYNTAX;
+        }
+    } else {
+        return ERROR_SYNTAX;
+    }
+}
+
+int params_2() {
+    // first check if params are empty or not
+    NEXT_TOKEN();
+    if (curr_token->type == TOK_RBRACKET) {
+        return ret;
+    } else if (curr_token->type == TOK_ID) { // params start correctly with ID
+        // now check the rest of the syntax and go to params_2_n if everything is correct
+        NEXT_TOKEN();
+        if (curr_token->type != TOK_COLON)
+            return ERROR_SYNTAX;
+        // next few lines of code check <types_keyword> rule
+        NEXT_TOKEN();
+        if ((curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_STRING) ||
+               (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_NUMBER) ||
+               (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_INTEGER)) {
+
+            return params_2_n();
+        } else {
+            return ERROR_SYNTAX;
+        }
+    } else {
+        return ERROR_SYNTAX;
+    }
+}
+
+int params_2_n() {
+    // check for end of params
+    NEXT_TOKEN();
+    if (curr_token->type == TOK_RBRACKET) {
+        return ret;
+    } else if (curr_token->type == TOK_COMMA) { // check for comma
+        // check the rest of the syntax and descend into parama_2_n if everything is correct
+        NEXT_TOKEN();
+        if (curr_token->type != TOK_ID)
+            return ERROR_SYNTAX;
+        NEXT_TOKEN();
+        if (curr_token->type != TOK_COLON)
+            return ERROR_SYNTAX;
+        // next lines check <types_keyword> rule
+        NEXT_TOKEN();
+        if ((curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_STRING) ||
+               (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_NUMBER) ||
+               (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_INTEGER)) {
+
+            return params_2_n();
+        } else {
+            return ERROR_SYNTAX;
+        }
+    } else {
+        return ERROR_SYNTAX;
+    }
+}
+
+int ret_params() {
+    NEXT_TOKEN();
+    if (curr_token->type != TOK_COLON) {
+        backup_token = curr_token;
+        return ret;
+    }
+    NEXT_TOKEN();
+    if ((curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_STRING) ||
+           (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_NUMBER) ||
+           (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_INTEGER)) {
+
+        return ret_params_n();
+    } else {
+        return ERROR_SYNTAX;
+    }
+}
+
+int ret_params_n() {
+    NEXT_TOKEN();
+    if (curr_token->type != TOK_COMMA) {
+        backup_token = curr_token;
+        return ret;
+    }
+    NEXT_TOKEN();
+    if ((curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_STRING) ||
+           (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_NUMBER) ||
+           (curr_token->type == TOK_KEYWORD && curr_token->attribute.keyword == KW_INTEGER)) {
+
+        return ret_params_n();
+    } else {
+        return ERROR_SYNTAX;
+    }
+}
+
+int body() {
+    //TODO: check if we have backup token, if there is one, dont read new token and
+    // just copy backup_token to curr_token and make backup_token NULL
+
 }
 
 int main() {
