a129594808920afbe0716bd9a376ed0951a81b4c
Adam Zvara <xzvara01@stud.fit.vutbr.cz>
2021-11-23 12:18:27 +0100

    generate more stuff
    created multiple new functions for generating code:
    
    generate_string - string in IFJcode21 format from string_t string
    (string@string), special cases for whitespaces, # and \ symbols
    
    generate_int - integer in IFJcode21 format (int@value)
    
    generate_decimal - float in IFJcode21 format (float@value)
    
    generate_entry - placed label for executing main() function in program
    
    generate_end/exit - skip generated builtin functions
    
    others - related to generating function, passing parameters etc...

--- src/generator.c
+++ src/generator.c
@@ -2,22 +2,53 @@
  * VUT IFJ Project 2021.
  *
  * @file generator.c
- * 
+ *
  * @brief Implementation of code generation (stack code)
- * 
- * @author Vojtěch Eichler 
+ *
+ * @author Vojtěch Eichler
  * @author Václav Korvas
  * @author Tomáš Matuš
  * @author Adam Zvara
  */
 
 #include <stdio.h>
-#include <string.h>
 #include "generator.h"
-#include "ibuffer.h"
 #include "str.h"
 
-void generate_name(local_symtab_t *local_tab, string_t name, string_t *output)
+void generate_string(string_t *insert_to, string_t string)
+{
+    str_insert(insert_to, "string@");
+
+    // special cases for whitespaces, hashtag and backslash
+    for (unsigned int i = 0; i < string.length; i++) {
+        if (string.str[i] <= ' ') {
+            str_insert(insert_to, "\\0");
+            str_insert_int(insert_to, string.str[i]);
+        } else if (string.str[i] == '#') {
+            str_insert(insert_to, "\\0");
+            str_insert_int(insert_to, string.str[i]);
+        } else if (string.str[i] == '\\') {
+            str_insert(insert_to, "\\0");
+            str_insert_int(insert_to, string.str[i]);
+        } else {
+            str_add_char(insert_to, string.str[i]);
+        }
+    }
+}
+
+void generate_int(string_t *insert_to, int number)
+{
+    str_insert(insert_to, "int@");
+    str_insert_int(insert_to, number);
+}
+
+void generate_decimal(string_t *insert_to, double number)
+{
+    str_insert(insert_to, "float@");
+    str_insert_double(insert_to, number);
+}
+
+void generate_name(string_t *output, string_t name)
 {
     str_insert(output, local_tab->key.str);
     str_insert(output, "$");
@@ -26,16 +57,37 @@ void generate_name(local_symtab_t *local_tab, string_t name, string_t *output)
     str_insert(output, name.str);
 }
 
-void generate_start()
-{ 
-    ADD_INST(".IFJcode21");
+void generate_entry()
+{
+    ADD_NEWLINE();
+    ADD_INST("label _start_");
     ADD_NEWLINE();
 }
 
-void generate_label(string_t name)
+void generate_start()
 {
+    ADD_INST(".IFJcode21");
+    ADD_NEWLINE();
+    ADD_INST("jump _start_");
+}
+
+void generate_end()
+{
+    ADD_INST("jump _end_");
+    ADD_NEWLINE();
+}
+
+void generate_exit()
+{
+    ADD_INST("label _end_");
+    ADD_NEWLINE();
+}
+
+void generate_label(string_t label_name)
+{
+    ADD_NEWLINE();
     ADD_INST("label ");
-    strcat(INST, name.str);
+    strcat(INST, label_name.str);
     ADD_NEWLINE();
 }
 
@@ -44,21 +96,21 @@ void generate_retvals()
     // storage for converting number to string
     string_t retval_num;
     str_init(&retval_num);
-    
+
     // item in global symtable corresponding to function
     struct global_item *func = global_find(global_tab, local_tab->key);
 
     // create local variables for return values in format LF@retval%N
-    // N is the position of return value    
+    // N is the position of return value
     for (int retval = 0; retval < str_len(func->retvals); retval++) {
         // define variable
-        ADD_INST("defvar LF@retval%");
+        ADD_INST("defvar LF@%retval");
         str_insert_int(&retval_num, retval);
         strcat(INST, retval_num.str);
         ADD_NEWLINE();
 
         // initialize its value to nil
-        ADD_INST("move LF@retval%");
+        ADD_INST("move LF@%retval");
         strcat(INST, retval_num.str);
         strcat(INST, " nil@nil");
 
@@ -77,15 +129,25 @@ void generate_parameters()
     // iterate through all identifiers
     // (at this point, only parameters are in local symtable)
     for (unsigned int i = 0; i < local_tab->size; i++) {
+        // variable definition
         ADD_INST("defvar LF@");
-
-        // create unique identificator name 
-        generate_name(local_tab, local_tab->data[i]->name, &id_mangled);
+        // create unique identificator name
+        generate_name(&id_mangled, local_tab->data[i]->name);
         strcat(INST, id_mangled.str);
+        ADD_NEWLINE();
 
+        // assign value from function call
+        ADD_INST("move LF@");
+        strcat(INST, id_mangled.str);
+        strcat(INST, " LF@%");
+        // id_mangled is now used to convert iterator variable into string
+        str_clear(&id_mangled);
+        str_insert_int(&id_mangled, i);
+        strcat(INST, id_mangled.str);
         ADD_NEWLINE();
 
         str_clear(&id_mangled);
+        ADD_NEWLINE();
     }
 
     str_free(&id_mangled);
@@ -93,8 +155,7 @@ void generate_parameters()
 
 void generate_function()
 {
-    ADD_NEWLINE(); // just so this part is separated from the previous
-
+    ADD_NEWLINE();
     generate_label(local_tab->key);
 
     // push previously set up temporary frame to frame stack
@@ -103,10 +164,123 @@ void generate_function()
     ADD_NEWLINE();
 
     generate_retvals();
+    ADD_NEWLINE();
     generate_parameters();
 }
 
-void generate_call()
+void generate_function_end()
 {
-    
+    ADD_INST("popframe");
+    ADD_NEWLINE();
+    ADD_INST("return");
+    ADD_NEWLINE();
+}
+
+void generate_identifier(string_t id_name)
+{
+    string_t id_mangled;
+    str_init(&id_mangled);
+
+    generate_name(&id_mangled, id_name);
+    ADD_INST("defvar LF@");
+    strcat(INST, id_mangled.str);
+    ADD_NEWLINE();
+
+    str_free(&id_mangled);
+}
+
+void generate_call_prep(func_def_t *f_helper)
+{
+    ADD_INST("createframe");
+    ADD_NEWLINE();
+
+    string_t param_name;
+    str_init(&param_name);
+    // generate generic names for function parameters
+    for (int i = 0; i < str_len(f_helper->item->params); i++) {
+        ADD_INST("defvar TF@");
+        str_insert(&param_name, "%");
+        str_insert_int(&param_name, i);
+        strcat(INST, param_name.str);
+        ADD_NEWLINE();
+        str_clear(&param_name);
+    }
+    str_free(&param_name);
+}
+
+void generate_params_const(token_t *token, func_def_t *f_helper)
+{
+    ADD_INST("move TF@");
+
+    string_t param_name;
+    str_init(&param_name);
+
+    str_insert(&param_name, "%");
+    str_insert_int(&param_name, f_helper->par_counter);
+    str_add_char(&param_name, ' ');
+    f_helper->par_counter++;
+
+    switch (token->type)
+    {
+    case TOK_STRING:
+        generate_string(&param_name, token->attribute.s);
+        break;
+
+    case TOK_INT:
+        generate_int(&param_name, token->attribute.number);
+        break;
+
+    case TOK_DECIMAL:
+        generate_decimal(&param_name, token->attribute.decimal);
+        break;
+
+    default:
+        break;
+    }
+
+    strcat(INST, param_name.str);
+    ADD_NEWLINE();
+    str_free(&param_name);
+}
+
+void generate_call(func_def_t *f_helper)
+{
+    ADD_INST("call ");
+    strcat(INST, f_helper->item->key.str);
+    ADD_NEWLINE();
+}
+
+void generate_write(token_t *token)
+{
+    string_t value;
+    str_init(&value);
+
+    ADD_INST("write ");
+
+    switch (token->type)
+    {
+    case TOK_STRING:
+        generate_string(&value, token->attribute.s);
+        break;
+
+    case TOK_INT:
+        generate_int(&value, token->attribute.number);
+        break;
+
+    case TOK_DECIMAL:
+        generate_int(&value, token->attribute.decimal);
+        break;
+
+    case TOK_ID:
+        str_insert(&value, "LF@");
+        generate_name(&value, token->attribute.s);
+
+    default:
+        break;
+    }
+
+    strcat(INST, value.str);
+    ADD_NEWLINE();
+
+    str_free(&value);
 }
--- src/generator.h
+++ src/generator.h
@@ -2,24 +2,27 @@
  * VUT IFJ Project 2021.
  *
  * @file generator.h
- * 
+ *
  * @brief Header file for generator.c
- * 
- * @author Vojtěch Eichler 
+ *
+ * @author Vojtěch Eichler
  * @author Václav Korvas
  * @author Tomáš Matuš
  * @author Adam Zvara
  */
 
-#ifndef _GENERATOR_H 
+#ifndef _GENERATOR_H
 #define _GENERATOR_H
 
 #define IBUFFER_SIZE 200  // size of ibuffer
 #define INSTR_SIZE   100  // size of single instruction
 
 #include <stdio.h>
+#include "scanner.h"
 #include "ibuffer.h"
 #include "symtable.h"
+#include "func_helper.h"
+
 
 extern local_symtab_t *local_tab;   // local symtable from parser
 extern global_symtab_t *global_tab; // global symtable from parser
@@ -27,19 +30,33 @@ extern ibuffer_t *buffer;           // instruction buffer from parser
 
 /**
  * @brief Generate name of identifier
- * @param local_tab Pointer to local symtable
- * @param name      Name of variable
+ * @param[in]  local_tab Pointer to local symtable
+ * @param[in]  name      Name of variable
+ * @param[out] output    Name of variable
  * @return Unique identifier name in format func_name$depth$id_name
  */
-void generate_name(local_symtab_t *local_tab, string_t name, string_t *output);
+void generate_name(string_t *output, string_t name);
 
 void generate_start();
+void generate_entry();
+void generate_end();
+void generate_exit();
 
-void generate_label(string_t name);
+void generate_label(string_t label_name);
 void generate_parameters();
 void generate_retvals();
 void generate_function();
+void generate_function_end();
 
-void generate_call();
+void generate_identifier(string_t id_name);
+
+void generate_call_prep(func_def_t *f_helper);
+void generate_params_const(token_t *token, func_def_t *f_helper);
+//void generate_params_id();
+void generate_call(func_def_t *f_helper);
+
+void generate_write(token_t *token);
+
+
 
 #endif // _GENERATOR_H
