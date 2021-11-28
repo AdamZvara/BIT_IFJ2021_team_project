a3b2f4db6f8f510694ebffa636ba1dee98f32ba3
Vojtech Eichler <vojta.eich@gmail.com>
2021-11-28 18:47:23 +0100

    tests: add colors to test output and remove bloat output from interpret

--- tests/parser_tests.sh
+++ tests/parser_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m'
 PARSER_DIR="error normal simple"
 for f in $PARSER_DIR; do
     PARSER_TEST_DIR=parser-tests/$f
@@ -14,20 +17,20 @@ for f in $PARSER_DIR; do
         EXPECTED=$PARSER_TEST_DIR/$(echo $f | cut -d'.' -f1).expected
         DIFF=$PARSER_TEST_DIR/$(echo $f | cut -d'.' -f1).diff
 
-        echo "Testing: $TEST_NAME"
+        echo -e "${BLUE}Testing:${NC} $TEST_NAME"
         if [ $PARSER_TEST_DIR == "parser-tests/error" ]; then
             # Run tests
             ../src/parser < $PARSER_TEST_DIR/$f >>/dev/null 2>&1
             if [ "$?" != $RETURN ]; then
-                echo "FAIL"
+                echo -e "${RED}FAIL${NC}"
             fi
         else
             # Run tests
             ../src/parser < $PARSER_TEST_DIR/$f > $OUTPUT
-            ./../interpret/ic21int $OUTPUT > $RESULT
+            ./../interpret/ic21int $OUTPUT > $RESULT 2>/dev/null
             diff $RESULT $EXPECTED > $DIFF
             if [ $? -ne 0 ]; then
-                echo "FAIL - CHECK .diff FILE"
+                echo -e "${RED}FAIL${NC} - CHECK .diff FILE"
             fi
         fi
 
