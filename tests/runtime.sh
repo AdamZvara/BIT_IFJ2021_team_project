9f02b0f7c1509a9d4f6eaef535104f7ffe8aab84
Adam Zvara <xzvara01@stud.fit.vutbr.cz>
2021-12-02 12:17:05 +0100

    add runtime error tests

--- tests/runtime.sh
+++ tests/runtime.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 ORANGE='\033[0;33m'
-PARSER_DIR="error failed_tests simple"
+PARSER_DIR="runtime"
 for f in $PARSER_DIR; do
     PARSER_TEST_DIR=parser-tests/$f
     PARSER_INPUT_FILES=$(ls $PARSER_TEST_DIR | grep .input)
@@ -19,24 +19,14 @@ for f in $PARSER_DIR; do
         DIFF=$PARSER_TEST_DIR/$(echo $f | cut -d'.' -f1).diff
 
         echo -e "${BLUE}Testing:${NC} $TEST_NAME"
-        if [ $PARSER_TEST_DIR == "parser-tests/error" ]; then
+        if [ $PARSER_TEST_DIR == "parser-tests/runtime" ]; then
             # Run tests
-            ../src/parser < $PARSER_TEST_DIR/$f >>/dev/null 2>&1
+            ../src/parser < $PARSER_TEST_DIR/$f > $OUTPUT
+            ./../interpret/ic21int $OUTPUT
             if [ "$?" != $RETURN ]; then
                 echo -e "${RED}FAIL${NC}"
             fi
-        else
-            # Run tests
-            ../src/parser < $PARSER_TEST_DIR/$f > $OUTPUT
-            ./../interpret/ic21int $OUTPUT > $RESULT 2>/dev/null
-            diff $RESULT $EXPECTED > $DIFF
-            if [ $? -ne 0 ]; then
-                echo -e "${RED}FAIL${NC} - CHECK .diff FILE"
-            fi
         fi
-
 
     done
 done
-
-bash runtime.sh
