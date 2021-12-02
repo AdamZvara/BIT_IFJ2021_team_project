e34794699ec5dc1e7cbfa3b30274a31addaf71b0
Korvas Vaclav <korvasv@seznam.cz>
2021-12-02 20:55:06 +0100

    Add script to run test that reads from stdin. And add more parser tests

--- tests/built_in.sh
+++ tests/built_in.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 ORANGE='\033[0;33m'
-PARSER_DIR="error failed_tests simple"
+PARSER_DIR="builtin"
 for f in $PARSER_DIR; do
     PARSER_TEST_DIR=parser-tests/$f
     PARSER_INPUT_FILES=$(ls $PARSER_TEST_DIR | grep .input)
@@ -17,27 +17,15 @@ for f in $PARSER_DIR; do
         RESULT=$PARSER_TEST_DIR/$(echo $f | cut -d'.' -f1).result
         EXPECTED=$PARSER_TEST_DIR/$(echo $f | cut -d'.' -f1).expected
         DIFF=$PARSER_TEST_DIR/$(echo $f | cut -d'.' -f1).diff
-
+		TEXT=$PARSER_TEST_DIR/$(echo $f | cut -d '.' -f1).txt
         echo -e "${BLUE}Testing:${NC} $TEST_NAME"
-        if [ $PARSER_TEST_DIR == "parser-tests/error" ]; then
-            # Run tests
-            ../src/parser < $PARSER_TEST_DIR/$f >>/dev/null 2>&1
-            if [ "$?" != $RETURN ]; then
-                echo -e "${RED}FAIL${NC}"
-            fi
-        else
-            # Run tests
-            ../src/parser < $PARSER_TEST_DIR/$f > $OUTPUT
-            ./../interpret/ic21int $OUTPUT > $RESULT 2>/dev/null
-            diff $RESULT $EXPECTED > $DIFF
-            if [ $? -ne 0 ]; then
-                echo -e "${RED}FAIL${NC} - CHECK .diff FILE"
-            fi
-        fi
-
+		# Run tests
+		../src/parser < $PARSER_TEST_DIR/$f > $OUTPUT
+		./../interpret/ic21int $OUTPUT < $TEXT > $RESULT 2>/dev/null
+		diff $RESULT $EXPECTED > $DIFF
+		if [ "$?" -ne 0 ]; then
+			echo -e "${RED}FAIL${NC} - CHECK .diff FILE" 
+		fi
 
     done
 done
-
-bash runtime.sh
-bash built_in.sh
